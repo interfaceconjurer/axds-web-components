d925240029817423c2da4ae81a602fb465321ce6
Brandon Ferrua <bferrua@gmail.com>
2021-01-22 14:54:45 -0800

    build: Add more boilerplate code

--- src/components/welcome-card/welcome-card.js
+++ src/components/welcome-card/welcome-card.js
@@ -1,11 +1,11 @@
-import template from "./welcome-card.html";
-import css from "./welcome-card.css";
+import template from './welcome-card.html';
+import css from './welcome-card.css';
 
 class WelcomeCard extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: "open" });
+    this.attachShadow({ mode: 'open' });
 
     this.shadowRoot.innerHTML = `
       <style>
