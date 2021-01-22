ac12d67ee11d6e532e1b3584c8ed7c7440cb130a
Brandon Ferrua <bferrua@gmail.com>
2021-01-22 15:51:08 -0800

    build: Change to use double quotes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   stories: [
     "../src/components/**/*.stories.mdx",
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../node_modules/@salesforce-ux/**/*.stories.js',
+    "../node_modules/@salesforce-ux/**/*.stories.js",
   ],
 
   webpackFinal: (config) => {
     // remove their css loader...
-    const rules = config.module.rules.filter((rule) => !rule.test.toString().match('.css'));
+    const rules = config.module.rules.filter((rule) => !rule.test.toString().match(".css"));
 
     // ...and replace with our own.
     rules.push({
       test: /\.css$/,
-      use: ['raw-loader'],
+      use: ["raw-loader"],
       include: [
-        path.resolve(__dirname, '../src/components'),
-        path.resolve(__dirname, '../node_modules'),
-        path.resolve(__dirname, '../node_modules/@salesforce-ux'),
+        path.resolve(__dirname, "../src/components"),
+        path.resolve(__dirname, "../node_modules"),
+        path.resolve(__dirname, "../node_modules/@salesforce-ux"),
       ],
       // lightdom assets go in exclude
       exclude: [],
@@ -26,16 +26,16 @@ module.exports = {
 
     rules.push({
       test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
+      use: ["style-loader", "css-loader"],
       // lightdom assets go in include
-      include: path.resolve(__dirname, '../src'),
-      exclude: path.resolve(__dirname, '../src/components'),
+      include: path.resolve(__dirname, "../src"),
+      exclude: path.resolve(__dirname, "../src/components"),
     });
 
     // markdown loader
     rules.push({
       test: /\.md$/,
-      use: ['markdown-loader'],
+      use: ["markdown-loader"],
     });
 
     // assign the updated rules to the config
